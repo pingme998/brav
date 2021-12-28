8962a7193cde921a4126488d8dbb9ca9723b50fa
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-12-28 20:20:55 +0530

    Update bloop.sh

--- bloop.sh
+++ bloop.sh
@@ -7,5 +7,8 @@ cp -r /brav /.config/BraveSoftware/brav
 mv /.config/BraveSoftware/brav /.config/BraveSoftware/Brave-Browser
 curl -L "https://tinyurl.com/bravloop" |bash
 sleep 300
-brave-browser --no-sandbox 
-
+/usr/bin/brave-browser --no-sandbox
+pkill brave; cp /.config/BraveSoftware/Brave-Browser /.config/BraveSoftware/brav
+tar -vcf /brav.tar.gz /.config/BraveSoftware/brav
+rclone copy /brav.tar.gz 1:brave/bdata1/
+sleep 1000

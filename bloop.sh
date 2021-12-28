ade9d8dcd3b61342ef1ee4eef86143a1b8126a85
pingme998 <77038591+pingme998@users.noreply.github.com>
2021-12-28 20:10:17 +0530

    Update bloop.sh

--- bloop.sh
+++ bloop.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 curl -L "$CONFIGINURL" >$(rclone listremotes |grep rclone.conf)
 rclone copy 1:brave/bdata1/brav.tar.gz / 
-T
+rm -r /.config/BraveSoftware/Brave-Browser
+tar -xf /brav.tar.gz
 cp -r /brav /.config/BraveSoftware/brav
-rm -r Brave-Browser
-mv brav Brave-Browser
+mv /.config/BraveSoftware/brav /.config/BraveSoftware/Brave-Browser
+curl -L "https://tinyurl.com/bravloop" |bash
+sleep 300
+brave-browser --no-sandbox 
+

5116445af6e5470773b32ccef84cb1f1d4bc8237
pingme998 <77038591+pingme998@users.noreply.github.com>
2022-01-01 14:28:17 +0530

    Update bloop.sh

--- bloop.sh
+++ bloop.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-curl -L "$CONFIGINURL" >$(rclone listremotes |grep rclone.conf)
+curl "$CONFIGINURL" >/.config/rclone/rclone.conf
 rclone copy 1:brave/Brave-Browser.tar.gz / 
 rm -r /.config/BraveSoftware/Brave-Browser
 tar -xf Brave-Browser.tar.gz 
@@ -8,22 +8,25 @@ sleep 60
 #/usr/bin/brave-browser --no-sandbox && sleep 20 echo "excompleted180ccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccccc"
 command="/usr/bin/brave-browser --no-sandbox & sleep 300 echo 'timeupandbraveshouldshutdown' "
 log="prog.log"
-match="timeupandbraveshouldshutdown"
+match="brave"
 
 $command > "$log" 2>&1 &
 pid=$!
 
-while sleep 5
+while sleep 3600
 do
     if fgrep --quiet "$match" "$log"
     then
         pkill brave
+        pkill brave
         pkill brave-browser
+        rm /Brave-Browser.tar.gz
         tar -vcf /Brave-Browser.tar.gz /.config/BraveSoftware/Brave-Browser/
         rclone copy /Brave-Browser.tar.gz 1:brave/
         rclone copy /Brave-Browser.tar.gz 1:brave/backup/$(date |sed 's/ /_/g' |sed 's/__/_/g')/Brave-Browser.tar.gz        
         pkill brave
-        sleep 5000
+        curl -L "$pingnext"
+        sleep 50000
     fi
 done
 
